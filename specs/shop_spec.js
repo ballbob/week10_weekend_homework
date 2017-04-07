b88383ca0b8c2710f9559586c26d57cb9cc66a7b
Robert Ball <robert.ball.1989@gmail.com>
2017-04-07 10:36:01 +0100

    Created record store spec.

---
+++ specs/shop_spec.js
@@ -0,0 +1,24 @@
+var assert=require('assert')
+var Record = require('../record.js')
+var Shop = require('../shop.js')
+
+describe('Record Store',function(){
+
+  var strangelove
+  var karmaChameleon
+  var theJubilee
+  var theSingingCricket
+
+  beforeEach(function(){
+
+    strangeLove = new Record('StrangeLove','Depeche Mode','Electronic',12)
+    karmaChameleon = new Record('Karma Chameleon','Culture Club','Pop',5)
+    theJubilee = new Record('The Jubilee','Mary Chapin Carpenter','Folk',10)
+
+    theSingingCricket = new Shop('The Singing Cricket','Bridge of Allan')
+  })
+
+  it('should have a name',function(){
+    assert.strictEqual('The Singing Cricket',theSingingCricket.name)
+  })
+})
